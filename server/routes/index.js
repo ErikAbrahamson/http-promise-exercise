5c83fb5ac94f844473dde67dc7d9e3ff0b55766d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-30 16:18:45 -0600

    add function arguments to promise

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var request = require('request');
 
-router.get('/promise', function(req, res, next) {
+router.post('/promise', function(req, res, next) {
+  var url = req.body;
   var successFn = function(data) { res.render('win'); };
-  var errorFn = function(status) { console.log('Error: ', status); };
+  var errorFn = function(status) { res.json(res.status); };
   var httpPromise = new Promise(function(resolve, reject) {
-    request('https://github.com/ErikAbrahamson', function(error, response, body) {
+    request(req.body, function(error, response, body) {
       if (error) reject(res.send('Shitty request. You did not enter a valid URL'));
       if (!error && response.statusCode == 200) resolve(successFn);
       else reject (errorFn);
